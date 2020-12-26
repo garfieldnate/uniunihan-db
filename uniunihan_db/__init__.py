ebce27018153db7eefb98f4ad079f8e029932506
Nathan Glenn <garfieldnate@gmail.com>
2020-12-26 19:47:44 +0100

    Update dependencies and add programmatic version
    For whatever reason, during development (like when running `poetry
    run build-db` or using the `poetry install`d version), importlib always
    fails to find the uniunihan_db module, immediately throwing an error.
    So we have two try statements in __init__.py: one for older pythons and
    another for the dev environment.
    
    Also update to python 3.9 so we can use the '|' syntax for combining
    dictionaries.

--- uniunihan_db/__init__.py
+++ uniunihan_db/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.0"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore # https://github.com/python/mypy/issues/1153
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except ModuleNotFoundError:  # happens during development or with `poetry install`'d module for some reason
+    __version__ = ""
