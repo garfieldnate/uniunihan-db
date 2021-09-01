8e28c2ff612e851b566a0ac699a2d216fac6729d
Nathan Glenn <garfieldnate@gmail.com>
2021-09-01 10:25:53 +0200

    Use hiragana for vocab pronunciations

--- uniunihan_db/format_book.py
+++ uniunihan_db/format_book.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+import jaconv
+import jinja2
 from jinja2 import Environment, FileSystemLoader
 
 TEMPLATES_DIR = Path(__file__).parent.parent / "templates"
@@ -8,6 +10,15 @@ JINJA_ENV = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 JINJA_ENV.trim_blocks = True
 JINJA_ENV.lstrip_blocks = True
 JINJA_ENV.keep_trailing_newline = False
+
+
+def kata2hira(s, default_value=""):
+    if isinstance(s, jinja2.runtime.Undefined):
+        return default_value
+    return jaconv.kata2hira(s)
+
+
+JINJA_ENV.filters["kata2hira"] = kata2hira
 
 GENERATED_DIR = Path(__file__).parent.parent / "data" / "generated"
 JP_DATA_FILE = GENERATED_DIR / "regularities" / "jp" / "final_output.json"
