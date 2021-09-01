f79908e8a8aedcf78267b7bd5f774088a5ce11e1
Nathan Glenn <garfieldnate@gmail.com>
2021-09-01 10:30:15 +0200

    add word breaks around slashes to prevent container overflow

--- uniunihan_db/format_book.py
+++ uniunihan_db/format_book.py
@@ -18,7 +18,14 @@ def kata2hira(s, default_value=""):
     return jaconv.kata2hira(s)
 
 
+def break_slashes(s, default_value=""):
+    if isinstance(s, jinja2.runtime.Undefined):
+        return default_value
+    return s.replace("/", "/<wbr>")
+
+
 JINJA_ENV.filters["kata2hira"] = kata2hira
+JINJA_ENV.filters["break_slashes"] = break_slashes
 
 GENERATED_DIR = Path(__file__).parent.parent / "data" / "generated"
 JP_DATA_FILE = GENERATED_DIR / "regularities" / "jp" / "final_output.json"
@@ -28,8 +35,6 @@ if __name__ == "__main__":
     jp_template = JINJA_ENV.get_template("jp_template.html.jinja")
     jp_data = json.load(JP_DATA_FILE.open("r"))
     # TODO: generate indices
-    # TODO: replace `/` with `/<wbr>` in English definitions
-    # TODO: ensure that ordering in maps is preserved
     result = jp_template.render(id=20, components=jp_data)
 
     print(result)
